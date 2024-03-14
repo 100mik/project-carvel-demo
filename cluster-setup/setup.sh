db0f7f5f0e11340822a1321fc52bad1e10fe656d
Daniel Garnier-Moiroux <git@garnier.wf>
2024-03-14 17:11:10 +0100

    app-cr,packaging,cluster-setup: use kind-registry.local

--- cluster-setup/setup.sh
+++ cluster-setup/setup.sh
@@ -47,11 +47,11 @@ fi
 
 
 # TODO: clean up
-reg_name='kind-registry'
-reg_port='5001'
+reg_name='kind-registry.local'
+reg_port='5000'
 if [ "$(docker inspect -f '{{.State.Running}}' "${reg_name}" 2>/dev/null || true)" != 'true' ]; then
   docker run \
-    -d --restart=always -p "127.0.0.1:${reg_port}:5000" --network bridge --name "${reg_name}" \
+    -d --restart=always -p "127.0.0.1:${reg_port}:5000" --name "${reg_name}" \
     registry:2
 fi
 
